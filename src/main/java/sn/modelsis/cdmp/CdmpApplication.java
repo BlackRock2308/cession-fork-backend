6b820a11680706c6f2319c6dec71bfafbac3fe02
Fatimetou Diagne <fmdiagne@modelsis.sn>
2022-07-13 12:35:54 +0000

    Initial commit

---
+++ src/main/java/sn/modelsis/cdmp/CdmpApplication.java
@@ -0,0 +1,13 @@
+package sn.modelsis.cdmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/modelsis/cdmp/CdmpApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.modelsis.cdmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
