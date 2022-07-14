61fa19e43d889ba5d919d7f51b1a8901a7de588b
kdieng <diengkhaly17@gmail.com>
2022-07-14 08:22:47 +0000

    create entities

--- src/main/java/sn/modelsis/cdmp/services/PmeService.java
+++ src/main/java/sn/modelsis/cdmp/services/PmeService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import sn.modelsis.cdmp.entities.Pme;
 import sn.modelsis.cdmp.repositories.PmeRepository;
 
+import java.util.Date;
+
 @Service
 public class PmeService {
     private PmeRepository pmeRepository;
@@ -14,13 +16,25 @@ public class PmeService {
     public void addPme(Pme pme) {
 
     }
-    public void updatePme(Pme pme, Long idPme) {
+    public Pme updatePme(Pme pme, Long idPme) {
         Pme pmeUpdate = pmeRepository.findById(idPme).get();
         pmeUpdate.setPrenomRepresentant(pme.getPrenomRepresentant());
         pmeUpdate.setNomRepresentant(pme.getNomRepresentant());
+        pmeUpdate.setRccm(pme.getRccm());
         pmeUpdate.setAdressePME(pme.getAdressePME());
-        pmeUpdate.setAtd(pme.getAtd());
         pmeUpdate.setTelephonePME(pme.getTelephonePME());
+        pmeUpdate.setDateImmatriculation(pme.getDateImmatriculation());
         pmeUpdate.setCentreFiscal(pme.getCentreFiscal());
+        pmeUpdate.setNinea(pme.getNinea());
+        pmeUpdate.setRaisonSocial(pme.getRaisonSocial());
+        pmeUpdate.setAtd(pme.getAtd());
+        pmeUpdate.setNantissement(pme.getNantissement());
+        pmeUpdate.setInterdictionBancaire(pme.getInterdictionBancaire());
+        pmeUpdate.setFormeJuridique(pme.getFormeJuridique());
+        pmeUpdate.setEmail(pme.getEmail());
+        pmeUpdate.setCodePin(pme.getCodePin());
+        pmeUpdate.setUrlImageProfile(pme.getUrlImageProfile());
+        pmeUpdate.setUrlImageSignature(pme.getUrlImageSignature());
+        return pmeRepository.save(pmeUpdate);
     }
 }
