0e78f357f11dabc8cef0758a6efa82303aa99b9e
diops <dsokhnadiarra@ept.sn>
2022-10-21 16:10:51 +0000

    Liste des demandes par statut

--- src/main/java/sn/modelsis/cdmp/repositories/DemandeRepository.java
+++ src/main/java/sn/modelsis/cdmp/repositories/DemandeRepository.java
@@ -2,6 +2,7 @@ package sn.modelsis.cdmp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sn.modelsis.cdmp.entities.Demande;
+import sn.modelsis.cdmp.entities.Pme;
 import sn.modelsis.cdmp.entities.Statuts;
 
 import java.util.List;
@@ -9,4 +10,8 @@ import java.util.List;
 public interface DemandeRepository extends JpaRepository<Demande, Long> {
 
     public List<Demande> findAllByStatut_Libelle(Statuts statut);
+
+    public List<Demande> findDemandeByPme(Pme pme);
+
+    public List<Demande> findAllByIdPME(Pme pme);
 }
