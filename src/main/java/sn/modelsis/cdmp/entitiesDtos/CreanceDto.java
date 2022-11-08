4f3a3b097242e746dc33396a3e712761784343cb
MBAYE SENE <smbaye@ept.sn>
2022-11-08 10:41:03 +0000

    adding enum of typeMarche

--- src/main/java/sn/modelsis/cdmp/entitiesDtos/CreanceDto.java
+++ src/main/java/sn/modelsis/cdmp/entitiesDtos/CreanceDto.java
@@ -1,11 +1,13 @@
 package sn.modelsis.cdmp.entitiesDtos;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import sn.modelsis.cdmp.entities.Statut;
+import sn.modelsis.cdmp.entities.TypeMarche;
 
 @Data
 @NoArgsConstructor
@@ -14,19 +16,27 @@ public class CreanceDto {
 
     private Long idCreance;
 
-    private String nomMarche; //Bon engagement
-
-    private String raisonSocial; //Pme
-
-    //private double soldePME; //Paiement
-
-    private double montantCreance; //Bon engagement
-
-    private Statut statut; //DemandeCession
+    private String ninea; //PME
 
     private String rccm; //PME
 
+    private String raisonSocial; //Pme
+
+    private TypeMarche typeMarche; //Bon engagement
+
+    private String nomMarche; //Bon engagement
+
+    private double montantCreance; //Bon engagement
+
+    private String decote; //Convention
+
+    private double soldePME; //Paiement
+
+    private Statut statut; //DemandeCession
+
     private Date dateDemandeCession; //DemandeCession
+
+    private LocalDateTime dateMarche; //DemandeCession
 
 
 
