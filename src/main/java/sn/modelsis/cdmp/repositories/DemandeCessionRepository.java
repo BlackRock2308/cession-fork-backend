282398cc495d7b8376f1beaf0dd5a8704eae77c5
MBAYE SENE <smbaye@ept.sn>
2022-11-16 15:27:14 +0000

    feat : adding multiparam research with data

--- src/main/java/sn/modelsis/cdmp/repositories/DemandeCessionRepository.java
+++ src/main/java/sn/modelsis/cdmp/repositories/DemandeCessionRepository.java
@@ -37,18 +37,29 @@ public interface DemandeCessionRepository extends JpaRepository<DemandeCession,L
 
     Page<DemandeCession> findAllByPmeIdPMEAndStatut_Libelle(org.springframework.data.domain.Pageable pageable,Long idPME, String statut);
 
+//    @Query("select p from DemandeCession as p where" +
+//            " (:referenceBE is null or p.bonEngagement.reference like %:referenceBE%) " +
+//            "or (:numeroDemande is null or p.numeroDemande like %:numeroDemande% )" +
+//            "or (:nomMarche is null or p.bonEngagement.nomMarche like %:nomMarche%)" +
+//            "or (:statutLibelle is null or p.statut.libelle like %:statutLibelle%) " +
+//            "or (:statutLibelle is null or p.statut.libelle like %:statutLibelle%) "
+//    )
+//    List<DemandeCession> findByReferenceBE(@Param("referenceBE") String referenceBE,
+//                                           @Param("numeroDemande") String numeroDemande,
+//                                           @Param("nomMarche") String nomMarche,
+//                                           @Param("statutLibelle") String statutLibelle);
+
     @Query("select p from DemandeCession as p where" +
             " (:referenceBE is null or p.bonEngagement.reference like %:referenceBE%) " +
             "or (:numeroDemande is null or p.numeroDemande like %:numeroDemande% )" +
             "or (:nomMarche is null or p.bonEngagement.nomMarche like %:nomMarche%)" +
-            "or (:statutLibelle is null or p.statut.libelle like %:statutLibelle%) " +
             "or (:statutLibelle is null or p.statut.libelle like %:statutLibelle%) "
+
     )
     List<DemandeCession> findByReferenceBE(@Param("referenceBE") String referenceBE,
                                            @Param("numeroDemande") String numeroDemande,
                                            @Param("nomMarche") String nomMarche,
                                            @Param("statutLibelle") String statutLibelle);
-
     @Query("select p from DemandeCession as p where p.dateDemandeCession between :startDate and :endDate")
     List<DemandeCession> findDemandeCessionByMyDate(@Param("startDate") LocalDateTime startDate,@Param("endDate") LocalDateTime endDate);
 
